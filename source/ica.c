fbd780b3e4d8435998003390a44cbad7579b1474
Adrien <26015179+ChuOkupai@users.noreply.github.com>
2019-01-13 00:56:49 +0100

    wow le turfu

---
+++ source/ica.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// Programme principal avec options pour contacter l'agence, avoir les missions et envoyer les choses demandées
+// Système de progression avec fichier de sauvegarde
+
+int main()
+{
+	puts("test");
+	return 0;
+}
